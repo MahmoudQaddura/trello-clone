ffdab46c5af1eb1bb922fa24be64b970fc2b878a
Mahmoud Qaddura <kmahmoud1898@gmail.com>
2023-07-20 15:42:57 +0300

    implemented drag and drop, Zustand. Set up Appwrite.

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,5 +1,37 @@
+"use client";
+import { useBoardStore } from "@/store/BoardStore";
+import { useEffect } from "react";
+import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
+import Column from "./Column";
 function Board() {
-  return <div>Board</div>;
+  const [board, getBoard] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  const handleOnDragEnd = (result: DropResult) => {};
+  console.log(board.columns.entries());
+
+  return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="grid grid-cols-1  md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+          >
+            {Array.from(board.columns.entries()).map(([id, column], index) => (
+              <Column key={id} id={id} todos={column.todos} index={index} />
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 }
 
 export default Board;
---
+++ store/BoardStore.ts
@@ -0,0 +1,15 @@
+import { getTodosGroupedByColumn } from "@/util/getTodosGroupedByColumn";
+import { create } from "zustand";
+
+interface BoardState {
+  board: Board;
+  getBoard: () => void;
+}
+
+export const useBoardStore = create<BoardState>((set) => ({
+  board: { columns: new Map<TypedColumn, Column>() },
+  getBoard: async () => {
+    const board = await getTodosGroupedByColumn();
+    set({ board });
+  },
+}));
